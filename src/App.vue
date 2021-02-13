34f5b73e0da2631e266a128bad0c3f3b46046d47
Elomena Idise <menadio1@gmail.com>
2021-02-13 17:46:40 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    @{{ user.username }} - {{ fullname }}
+    <strong></strong>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "itzmena",
+        firstname: "Elomena",
+        lastname: "Onome",
+        email: "hellodevelo@gmail.com",
+        isAdmin: true,
+      },
+    };
+  },
+
+  computed: {
+    fullname() {
+      return `${this.user.firstname} ${this.user.lastname}`;
+    },
+  },
+
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
